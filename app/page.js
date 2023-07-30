c98d52ff7a66903d72f3fc92462236d023f42f54
ItsFaizan <faizanjavid71@gmail.com>
2023-07-30 20:26:03 +0500

    Starting Page added

---
+++ app/page.js
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-7xl items-start pb-5 pt-28 pl-20">
+        Hey, It's me <span className="text-[#1fbcff] "> Faizan Javed </span> 
+      </div>
+      <h2 className="pl-20 text-3xl">I am practing TailWind CSS with NextJS</h2>
+      <div className="pt-16">
+        <h1 className="text-4xl text-center pt-9 font-semibold pr-5 text-[#1fbcff]">About me</h1>
+        <p className="text-center text-2xl pt-5">I am a web developer and I am learning NextJS and TailWind CSS. <br/>I am also studing Software Engineering from Fast Nuces Islamabad.<br/>I am currently working in MERN Stack and also aiming to do my FYP in it .</p>
+        <div className="text-center pt-9">
+        <button className="text-3xl bg-[#1fbcff] pt-2 pb-2 pl-2 pr-2 ">Contact ME!</button>
+        </div>
+      </div>  
+      </>
+  )
+}
